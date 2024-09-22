9c4f3e36dcdb91bfd066c447bf1d9cf3f2a03e01
seohyeon1578 <seohyeon1578@naver.com>
2024-09-22 16:10:06 +0900

    feat: constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Dante's Blog";
+export const SITE_DESCRIPTION =
+  "개발일을 하면서 겪은 경험을 기록하는 공간입니다.";
