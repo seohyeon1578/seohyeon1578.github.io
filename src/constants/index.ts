de81ac21e77598575a3f4f0004cc1a3380fba0e4
김서현 <seohyeon1578@naver.com>
2024-09-22 17:45:45 +0900

    feat: layout component (#1)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,7 @@
 export const SITE_TITLE = "Dante's Blog";
 export const SITE_DESCRIPTION =
   "개발일을 하면서 겪은 경험을 기록하는 공간입니다.";
+
+export const SOCIAL_GITHUB = "https://github.com/seohyeon1578";
+export const SOCIAL_LINKEDIN = "https://www.linkedin.com/in/seohyeon-kim-b290bb243/";
+export const SOCIAL_GMAIL = "mailto:gseohyeon07@gmail.com";
