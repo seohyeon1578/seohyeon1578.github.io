530793655007163aefba73f15e9ce2aaff99c71b
seohyeon1578 <seohyeon1578@naver.com>
2024-10-09 18:22:41 +0900

    fix: favicon and page title, descriptions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,9 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 import type { WorkExperience } from "../@types/WorkExperience";
 import MissgoLogo from "../assets/companyLogo/missgo-logo.jpg";
 import StartupLogo from "../assets/companyLogo/startup_logo.jpg";
 
 export const SITE_TITLE = "Dante's Blog";
+export const ABOUT_TITLE = "김서현 | Dante Kim | Dante's Blog";
 export const SITE_DESCRIPTION =
   "개발일을 하면서 겪은 경험을 기록하는 공간입니다.";
 
