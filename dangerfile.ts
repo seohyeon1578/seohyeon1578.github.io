215092c2e801798678b63a8f96d0a4c48567e8a0
seohyeon1578 <seohyeon1578@naver.com>
2024-11-14 21:50:50 +0900

    fix(ci/cd): danget ci

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,17 +4,17 @@ import { promisify } from 'util'
 
 const execAsync = promisify(exec)
 
-async function checkFormatting() {
+async function checkFormatting(): Promise<void> {
   try {
     await execAsync('npm run format:check')
-  } catch (error) {
-    const files = danger.git.modified_files.concat(danger.git.created_files)
-    const formattingIssues = files.filter(file => 
+  } catch (error: any) {
+    const files: string[] = danger.git.modified_files.concat(danger.git.created_files)
+    const formattingIssues: string[] = files.filter(file => 
       error.stdout.includes(file)
     )
 
     if (formattingIssues.length > 0) {
-      const formattedFileList = formattingIssues.map(file => `- ${file}`).join('\n')
+      const formattedFileList: string = formattingIssues.map(file => `- ${file}`).join('\n')
       warn(`다음 파일들의 포맷팅이 필요합니다:\n${formattedFileList}\n\n코드를 포맷팅하려면 \`npm run format\`을 실행해주세요.`)
     }
   }
