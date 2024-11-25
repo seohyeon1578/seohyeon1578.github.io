20b5c92f6f2aca7549da494a0d890935ec2d8550
seohyeon1578 <seohyeon1578@naver.com>
2024-11-25 18:26:04 +0900

    fix: rss xml file

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,17 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import type { APIRoute } from "astro";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../constants";
 
-export async function GET(context) {
+export const GET: APIRoute = async ({ site }) => {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: site ?? "",
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
     })),
   });
-}
+};
